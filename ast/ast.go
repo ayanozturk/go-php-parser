f98b03711e28b0245e3cc670ad4cefb1ece48959
Ayan Ozturk <ayan.ozturk@rewardgateway.com>
2025-03-17 17:32:22 +0000

    remove InterfaceNode and InterfaceMethodNode definitions from ast.go

--- ast/ast.go
+++ ast/ast.go
@@ -501,31 +501,6 @@ func (c *CommentNode) TokenLiteral() string {
 	return c.Value
 }
 
-// InterfaceNode represents a PHP interface definition
-type InterfaceNode struct {
-	Name    string
-	Methods []Node
-	Pos     Position
-}
-
-func (i *InterfaceNode) NodeType() string    { return "Interface" }
-func (i *InterfaceNode) GetPos() Position    { return i.Pos }
-func (i *InterfaceNode) SetPos(pos Position) { i.Pos = pos }
-func (i *InterfaceNode) String() string {
-	var parts []string
-	parts = append(parts, fmt.Sprintf("Interface(%s)", i.Name))
-	if len(i.Methods) > 0 {
-		parts = append(parts, "Methods:")
-		for _, method := range i.Methods {
-			parts = append(parts, "  "+method.String())
-		}
-	}
-	return fmt.Sprintf("%s @ %d:%d", strings.Join(parts, "\n"), i.Pos.Line, i.Pos.Column)
-}
-func (i *InterfaceNode) TokenLiteral() string {
-	return "interface"
-}
-
 // ClassNode represents a PHP class definition
 type ClassNode struct {
 	Name       string
@@ -613,31 +588,4 @@ func (m *MethodCallNode) String() string {
 }
 func (m *MethodCallNode) TokenLiteral() string {
 	return m.Method
-}
-
-// InterfaceMethodNode represents a method declaration in an interface
-type InterfaceMethodNode struct {
-	Name       string
-	Visibility string // public, private, protected
-	ReturnType string
-	Params     []Node
-	Pos        Position
-}
-
-func (m *InterfaceMethodNode) NodeType() string    { return "InterfaceMethod" }
-func (m *InterfaceMethodNode) GetPos() Position    { return m.Pos }
-func (m *InterfaceMethodNode) SetPos(pos Position) { m.Pos = pos }
-func (m *InterfaceMethodNode) String() string {
-	var parts []string
-	if m.Visibility != "" {
-		parts = append(parts, m.Visibility)
-	}
-	parts = append(parts, fmt.Sprintf("Method(%s)", m.Name))
-	if m.ReturnType != "" {
-		parts = append(parts, fmt.Sprintf(": %s", m.ReturnType))
-	}
-	return fmt.Sprintf("%s @ %d:%d", strings.Join(parts, " "), m.Pos.Line, m.Pos.Column)
-}
-func (m *InterfaceMethodNode) TokenLiteral() string {
-	return "function"
 }
