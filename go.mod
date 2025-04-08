f1ac32d02cacfce944dcbd583d44c29b0d4f174d
Ayan Ozturk <ayan.ozturk@rewardgateway.com>
2025-04-08 19:22:37 +0100

    start adding config structure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-phpcs
 
 go 1.23
+
+require gopkg.in/yaml.v2 v2.4.0
--- main.go
+++ main.go
@@ -6,20 +6,63 @@ import (
 	"go-phpcs/command"
 	"go-phpcs/lexer"
 	"go-phpcs/parser"
+	"log"
 	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
 )
 
+type Config struct {
+	Path       string   `yaml:"path"`
+	Extensions []string `yaml:"extensions"`
+	Ignore     []string `yaml:"ignore"`
+}
+
 func main() {
+	// Load the configuration
+	config, err := loadConfig("config.yaml")
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	filesToScan, err := getFilesToScan(config)
+	if err != nil {
+		log.Fatalf("Error scanning files: %v", err)
+	}
+
+	// Print the files to scan
+	// fmt.Println("Files to scan:")
+	// for _, file := range filesToScan {
+	// 	fmt.Println(file)
+	// }
+
 	debug := flag.Bool("debug", false, "Enable debug mode to show parsing errors")
 	flag.Parse()
 
-	if len(flag.Args()) < 2 {
+	if len(flag.Args()) < 2 && len(filesToScan) == 0 {
+		fmt.Println("Usage: go-phpcs <command> <file>")
 		command.PrintUsage()
 		os.Exit(1)
 	}
 
-	commandName := flag.Args()[0]
-	filePath := flag.Args()[1]
+	commandName := "ast"
+	if len(flag.Args()) > 0 {
+		commandName = flag.Args()[0]
+	}
+
+	filePath := ""
+	if len(flag.Args()) > 1 {
+		filePath = flag.Args()[1]
+	} else {
+		filePath = filesToScan[0]
+	}
+	fmt.Println(filePath)
+	if filePath == "" {
+		fmt.Println("No file specified for parsing.")
+		command.PrintUsage()
+		os.Exit(1)
+	}
 
 	// Read the PHP file
 	input, err := os.ReadFile(filePath)
@@ -66,4 +109,51 @@ func main() {
 		command.PrintUsage()
 		os.Exit(1)
 	}
+}
+
+func loadConfig(filename string) (*Config, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var config Config
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func getFilesToScan(config *Config) ([]string, error) {
+	var filesToScan []string
+
+	err := filepath.Walk(config.Path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip ignored directories
+		for _, ignore := range config.Ignore {
+			if info.IsDir() && info.Name() == ignore {
+				return filepath.SkipDir
+			}
+		}
+
+		// Check file extensions
+		if !info.IsDir() {
+			for _, ext := range config.Extensions {
+				if filepath.Ext(path) == "."+ext {
+					filesToScan = append(filesToScan, path)
+					break
+				}
+			}
+		}
+
+		return nil
+	})
+
+	return filesToScan, err
 }
