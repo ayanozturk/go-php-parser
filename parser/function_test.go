1a4678c1cadf62201b1a72ba39564bf91f3900cc
Ayan Ozturk <ayan.ozturk@rewardgateway.com>
2025-04-26 14:23:25 +0100

    Refactor parser: Move function parsing logic from parser.go to function.go for better modularity and maintainability. No functional changes.

--- parser/function_test.go
+++ parser/function_test.go
@@ -180,3 +180,22 @@ function bar(array|string|null $x = null, ?string $y = "abc"): array|string|fals
 		t.Error("Expected at least one node, but got none")
 	}
 }
+
+func TestParseFunctionWithParenthesizedTypeParam(t *testing.T) {
+	input := `<?php
+class X {
+    public function setParent((NodeDefinition&ParentNodeDefinitionInterface)|null $parent): static {}
+}`
+
+	l := lexer.New(input)
+	p := New(l, true)
+	nodes := p.Parse()
+
+	if len(p.Errors()) > 0 {
+		t.Errorf("Parser returned errors: %v", p.Errors())
+	}
+
+	if len(nodes) == 0 {
+		t.Error("Expected at least one node, but got none")
+	}
+}
