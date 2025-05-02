753fc781d3c80b95a10e814aada00b11bdc438eb
Ayan Ozturk <ayan.ozturk@rewardgateway.com>
2025-05-02 10:23:32 +0100

    Improve NoMultipleStatementsPerLineChecker: robustly skip semicolons inside multiline heredoc/nowdoc, block comments, and all string/comment types for accurate PSR-12 compliance

--- style/psr12/rule_no_multiple_statements.go
+++ style/psr12/rule_no_multiple_statements.go
@@ -9,6 +9,9 @@ type NoMultipleStatementsPerLineChecker struct{}
 
 func (c *NoMultipleStatementsPerLineChecker) CheckIssues(lines []string, filename string) []style.StyleIssue {
 	var issues []style.StyleIssue
+	inBlockComment := false
+	inHeredoc := false
+	heredocEnd := ""
 	for i, line := range lines {
 		trimmed := line
 		if len(trimmed) == 0 {
@@ -20,10 +23,75 @@ func (c *NoMultipleStatementsPerLineChecker) CheckIssues(lines []string, filenam
 			continue
 		}
 		count := 0
-		for j := 0; j < len(line); j++ {
-			if line[j] == ';' {
+		inSingle := false
+		inDouble := false
+		j := 0
+		for j < len(line) {
+			if inBlockComment {
+				if j+1 < len(line) && line[j] == '*' && line[j+1] == '/' {
+					inBlockComment = false
+					j += 2
+					continue
+				}
+				j++
+				continue
+			}
+			if inHeredoc {
+				// Heredoc/nowdoc ends only if the line (after optional whitespace) matches the marker
+				k := 0
+				for k < len(line) && (line[k] == ' ' || line[k] == '\t') { k++ }
+				if heredocEnd != "" && line[k:] == heredocEnd {
+					inHeredoc = false
+					heredocEnd = ""
+				}
+				break // skip the rest of this line
+			}
+			if !inSingle && !inDouble {
+				// Line comments
+				if j+1 < len(line) && line[j] == '/' && line[j+1] == '/' {
+					break
+				}
+				if line[j] == '#' {
+					break
+				}
+				// Block comment start
+				if j+1 < len(line) && line[j] == '/' && line[j+1] == '*' {
+					inBlockComment = true
+					j += 2
+					continue
+				}
+				// Heredoc/Nowdoc start
+				if j+2 < len(line) && line[j] == '<' && line[j+1] == '<' && line[j+2] == '<' {
+					k := j + 3
+					for k < len(line) && (line[k] == ' ' || line[k] == '\t') { k++ }
+					start := k
+					for k < len(line) && ((line[k] >= 'a' && line[k] <= 'z') || (line[k] >= 'A' && line[k] <= 'Z') || (line[k] >= '0' && line[k] <= '9') || line[k] == '_' || line[k] == '\'' || line[k] == '"') { k++ }
+					heredocEnd = line[start:k]
+					if heredocEnd != "" {
+						inHeredoc = true
+						j = k
+						break // rest of line is part of heredoc start
+					}
+				}
+			}
+			if !inDouble && line[j] == '\'' {
+				inSingle = !inSingle
+				j++
+				continue
+			}
+			if !inSingle && line[j] == '"' {
+				inDouble = !inDouble
+				j++
+				continue
+			}
+			if line[j] == '\\' { // skip escaped quotes
+				j += 2
+				continue
+			}
+			if !inSingle && !inDouble && !inBlockComment && !inHeredoc && line[j] == ';' {
 				count++
 			}
+			j++
 		}
 		if count > 1 {
 			issues = append(issues, style.StyleIssue{
