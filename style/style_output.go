e7c8246d0d5fd7c5c5d7b9db83ad2fd45b22d46e
Ayan Ozturk <ayan.ozturk@rewardgateway.com>
2025-05-04 15:15:24 +0100

    Refactor: reduce cognitive complexity of PrintPHPCSStyleOutputToWriter by extracting helpers (SonarQube S3776)

--- style/style_output.go
+++ style/style_output.go
@@ -18,55 +18,67 @@ func PrintPHPCSStyleOutputToWriter(w io.Writer, issues []StyleIssue) {
 	if len(issues) == 0 {
 		return
 	}
-	// Group by file
+	fileMap := groupIssuesByFile(issues)
+	files := sortedFileNames(fileMap)
+	totalErrors := 0
+	for _, file := range files {
+		fileIssues := fileMap[file]
+		errCount, warnCount, totalLines := countFileIssues(fileIssues)
+		totalErrors += errCount
+		printFileIssues(w, file, fileIssues, errCount, warnCount, totalLines)
+	}
+	fmt.Fprintf(w, "Run summary: total style errors found: %d\n", totalErrors)
+}
+
+func groupIssuesByFile(issues []StyleIssue) map[string][]StyleIssue {
 	fileMap := map[string][]StyleIssue{}
 	for _, iss := range issues {
 		fileMap[iss.Filename] = append(fileMap[iss.Filename], iss)
 	}
+	return fileMap
+}
 
+func sortedFileNames(fileMap map[string][]StyleIssue) []string {
 	files := make([]string, 0, len(fileMap))
 	for f := range fileMap {
 		files = append(files, f)
 	}
 	sort.Strings(files)
+	return files
+}
 
-	totalErrors := 0
-
-	for _, file := range files {
-		fileIssues := fileMap[file]
-		errCount, warnCount := 0, 0
-		lineSet := map[int]struct{}{}
-		for _, iss := range fileIssues {
-			if iss.Type == Error {
-				errCount++
-			} else {
-				warnCount++
-			}
-			lineSet[iss.Line] = struct{}{}
+func countFileIssues(fileIssues []StyleIssue) (errCount, warnCount, totalLines int) {
+	lineSet := map[int]struct{}{}
+	for _, iss := range fileIssues {
+		if iss.Type == Error {
+			errCount++
+		} else {
+			warnCount++
 		}
-		totalLines := len(lineSet)
-		totalErrors += errCount
-		fmt.Fprintf(w, "FILE: %s\n", file)
-		fmt.Fprintln(w, strings.Repeat("-", 80))
-		fmt.Fprintf(w, "FOUND %d ERRORS AND %d WARNING%s AFFECTING %d LINE%s\n", errCount, warnCount, plural(warnCount), totalLines, plural(totalLines))
-		fmt.Fprintln(w, strings.Repeat("-", 80))
-		// Sort by line
-		sort.Slice(fileIssues, func(i, j int) bool {
-			if fileIssues[i].Line == fileIssues[j].Line {
-				return fileIssues[i].Column < fileIssues[j].Column
-			}
-			return fileIssues[i].Line < fileIssues[j].Line
-		})
-		for _, iss := range fileIssues {
-			fmt.Fprintf(w, "%4d | %-7s | %s\n", iss.Line, iss.Type, iss.Message)
-			if iss.Code != "" {
-				fmt.Fprintf(w, "     |         |     (%s)\n", iss.Code)
-			}
-		}
-		fmt.Fprintln(w)
+		lineSet[iss.Line] = struct{}{}
 	}
-	// Print run summary
-	fmt.Fprintf(w, "Run summary: total style errors found: %d\n", totalErrors)
+	totalLines = len(lineSet)
+	return
+}
+
+func printFileIssues(w io.Writer, file string, fileIssues []StyleIssue, errCount, warnCount, totalLines int) {
+	fmt.Fprintf(w, "FILE: %s\n", file)
+	fmt.Fprintln(w, strings.Repeat("-", 80))
+	fmt.Fprintf(w, "FOUND %d ERRORS AND %d WARNING%s AFFECTING %d LINE%s\n", errCount, warnCount, plural(warnCount), totalLines, plural(totalLines))
+	fmt.Fprintln(w, strings.Repeat("-", 80))
+	sort.Slice(fileIssues, func(i, j int) bool {
+		if fileIssues[i].Line == fileIssues[j].Line {
+			return fileIssues[i].Column < fileIssues[j].Column
+		}
+		return fileIssues[i].Line < fileIssues[j].Line
+	})
+	for _, iss := range fileIssues {
+		fmt.Fprintf(w, "%4d | %-7s | %s\n", iss.Line, iss.Type, iss.Message)
+		if iss.Code != "" {
+			fmt.Fprintf(w, "     |         |     (%s)\n", iss.Code)
+		}
+	}
+	fmt.Fprintln(w)
 }
 
 // PrintPHPCSStyleIssueToWriter prints a single StyleIssue in PHPCS format to the provided writer.
