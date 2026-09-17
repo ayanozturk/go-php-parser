8c2195c8ee5bea9ffa4a53ff1b09a5b0cc7823cf
ayan.ozturk <ayan.ozturk@rewardgateway.com>
2026-09-17 10:27:51 +0100

    chore: strip committed binaries, gob cache, and debris files from git
    - Untrack and delete php-parser binary, cover.out, config.yaml.bak,
      scanner.sh, demo_constants.php, test_elseif.php, test_elseif2.php,
      test_control_structure_spacing.php
    - Untrack and delete 49 ast/*.gob runtime AST cache files (regenerated
      on demand by ASTCacheManager, not test fixtures)
    - Update .gitignore to prevent recurrence

--- test_control_structure_spacing.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($condition){
-    echo "test";
-}
-for  ($i = 0; $i < 10; $i++)  {
-    echo $i;
-}
-myFunction ($param);
--- test_elseif2.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ($test) {
-    echo "test";
-} else if ($other) {
-    echo "other";
-}
